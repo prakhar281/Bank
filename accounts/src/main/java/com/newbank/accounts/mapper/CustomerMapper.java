2eb9dce64c544912e1d61c78753998ce99c440d1
Prakhar Sharma <PR20317305@wipro.com>
2025-10-05 20:01:40 +0530

    Initial commit

---
+++ accounts/src/main/java/com/newbank/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,19 @@
+package com.newbank.accounts.mapper;
+
+import com.newbank.accounts.dto.CustomerDto;
+import com.newbank.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto){
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer){
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
